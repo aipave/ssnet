a815bbabfaad0c978db0c564a0f1c43ded0c7c2c
PeiChen Lyu <aiplayer06@gmail.com>
2019-05-17 12:50:49 +0800

    Refacotor: build.sh

--- build.sh
+++ build.sh
@@ -6,11 +6,16 @@ BUILD_TYPE=${BUILD_TYPE:-release}
 INSTALL_DIR=${INSTALL_DIR:-./${BUILD_TYPE}-install}
 CXX=${CXX:-clang++}
 
-mkdir -p "$BUILD_DIR/$BUILD_TYPE" \
-    && cd "$BUILD_DIR/$BUILD_TYPE" \
-    && cmake \
+if [ ! -d "$BUILD_DIR/$BUILD_TYPE" ]; then
+    mkdir -p "$BUILD_DIR/$BUILD_TYPE" || exit 1
+fi
+
+cd "$BUILD_DIR/$BUILD_TYPE" || exit 1
+
+cmake \
         -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
         -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
         "$SOURCE_DIR" \
-    && make -j16 "$@"
+
+make -j16 "$@"
